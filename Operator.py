5010c9803b3626a2122ebeca72ff246d7b567024
Shailesh Sahu <sahushailesh9@gmail.com>
2018-09-22 09:22:24 -0400

    Operator learning subject concept

---
+++ Operator.py
@@ -0,0 +1,58 @@
+# Here we going to learn operator in the python which little
+# bit different in other programming language hope we 
+# will got to learn that language in fast in simple manner
+
+
+# Arithmetic Operator
+
+print("Enter the first number")
+x=float(input())
+print("Enter the second number")
+y=float(input())
+
+print(x+y)
+print(x-y)
+print(x/y)
+print(x*y)
+print(x%y)
+print(x//y)
+print(x**y)
+
+
+#assignment operator
+print("Assignment Operator")
+
+x+=.56
+x-=.2
+x*=.345
+x/=2
+
+print(5 << 3)
+print(3 and 5)
+
+y=20
+if(y,y==30):
+	print(y)		
+
+
+#logical operator
+x=10
+y=0
+print(x and y)
+print(x or y)
+print(not y)
+
+
+#identifier operator in python
+
+array1 =["a","b","c"]
+array2 =["a","b","c"]
+array3=array1
+print(array3 is array1)
+print(array1 is array2)
+print(array2 is not array3)
+print("a" in array2)
+print("d" not in array3)
+
+
+
